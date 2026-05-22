de2d1b319776ec556652095a2a3b2ad399ee4f74
Ggowthampatnaik <161194915+Ggowthampatnaik@users.noreply.github.com>
2026-05-22 23:01:06 +0530

    Add initial Glue job implementation

---
+++ glue/glue_job.py
@@ -0,0 +1,23 @@
+import sys
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+print("Glue Job Started")
+
+# Read data from Redshift example
+# Add your transformation logic here
+
+print("Glue Job Completed Successfully")
+
+job.commit()
