8091dbe3e4a6774388e41a2aed9ef163b90b9527
Ggowthampatnaik <161194915+Ggowthampatnaik@users.noreply.github.com>
2026-05-27 20:03:31 +0530

    Update glue_job.py

--- glue/glue_job.py
+++ glue/glue_job.py
@@ -1,6 +1,4 @@
 import sys
-import json
-import boto3
 
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
@@ -9,55 +7,41 @@ from awsglue.job import Job
 
 args = getResolvedOptions(
     sys.argv,
-    ["JOB_NAME", "secret_name", "db_table", "output_path"]
+    [
+        "JOB_NAME",
+        "rds_endpoint",
+        "db_name",
+        "db_username",
+        "db_password",
+        "db_table",
+        "output_path"
+    ]
 )
 
 sc = SparkContext()
+
 glueContext = GlueContext(sc)
+
 spark = glueContext.spark_session
 
 job = Job(glueContext)
+
 job.init(args["JOB_NAME"], args)
 
 print("Glue Job Started")
 
-client = boto3.client("secretsmanager")
-secret_response = client.get_secret_value(SecretId=args["secret_name"])
-secret = json.loads(secret_response["SecretString"])
-
-host = secret["host"]
-port = secret["port"]
-database = secret["database"]
-username = secret["username"]
-password = secret["password"]
-
-print("Secret loaded")
-print(f"Host: {host}")
-print(f"Database: {database}")
-print(f"Table: {args['db_table']}")
-
-jdbc_url = f"jdbc:mysql://{host}:{port}/{database}?connectTimeout=10000&socketTimeout=30000&useSSL=false"
-
-connection_properties = {
-    "user": username,
-    "password": password,
-    "driver": "com.mysql.cj.jdbc.Driver",
-    "fetchsize": "1000"
-}
-
-print("Testing MySQL connection with COUNT query")
-
-count_query = f"(SELECT COUNT(*) AS row_count FROM {args['db_table']}) AS count_check"
-
-count_df = spark.read.jdbc(
-    url=jdbc_url,
-    table=count_query,
-    properties=connection_properties
+jdbc_url = (
+    f"jdbc:mysql://{args['rds_endpoint']}:3306/"
+    f"{args['db_name']}?useSSL=false"
 )
 
-count_df.show()
+connection_properties = {
+    "user": args["db_username"],
+    "password": args["db_password"],
+    "driver": "com.mysql.cj.jdbc.Driver"
+}
 
-print("Reading table")
+print("Reading MySQL Table")
 
 df = spark.read.jdbc(
     url=jdbc_url,
@@ -65,17 +49,15 @@ df = spark.read.jdbc(
     properties=connection_properties
 )
 
-df.show(10)
+df.show()
 
-print("Writing output")
+print("Writing Output to S3")
 
 df.write \
     .mode("overwrite") \
     .option("header", "true") \
     .csv(args["output_path"])
 
-print("Data Written to S3")
+print("Glue Job Completed")
 
 job.commit()
-
-print("Glue Job Completed")
