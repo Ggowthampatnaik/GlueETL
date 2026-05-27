6739d60facece30fd981da7e62bc3b8a77013c03
Ggowthampatnaik <161194915+Ggowthampatnaik@users.noreply.github.com>
2026-05-27 18:31:46 +0530

    Update glue_job.py

--- glue/glue_job.py
+++ glue/glue_job.py
@@ -1,128 +1,70 @@
 import sys
+import json
+import boto3
 
-from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-from awsglue.dynamicframe import DynamicFrameCollection
-from awsglue.dynamicframe import DynamicFrame
-from awsgluedq.transforms import EvaluateDataQuality
+from pyspark.sql.functions import col
 
-# ---------------------------------------------------
-# Custom Transform
-# ---------------------------------------------------
-
-def MyTransform(glueContext, dfc) -> DynamicFrameCollection:
-    from pyspark.sql.functions import col
-    from awsglue.dynamicframe import DynamicFrame, DynamicFrameCollection
-
-    df = dfc.select(list(dfc.keys())[0]).toDF()
-
-    df = df.withColumn("quantity", col("quantity") + 1)
-
-    df = df.coalesce(1)
-
-    result = DynamicFrame.fromDF(df, glueContext, "result")
-
-    return DynamicFrameCollection({"CustomTransform0": result}, glueContext)
-
-
-# ---------------------------------------------------
-# Glue Job Init
-# ---------------------------------------------------
-
-args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+args = getResolvedOptions(
+    sys.argv,
+    [
+        "JOB_NAME",
+        "secret_name",
+        "db_table",
+        "output_path"
+    ]
+)
 
 sc = SparkContext()
-
 glueContext = GlueContext(sc)
-
 spark = glueContext.spark_session
 
 job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
 
-job.init(args['JOB_NAME'], args)
+print("Glue Job Started")
 
-# ---------------------------------------------------
-# Data Quality Rules
-# ---------------------------------------------------
-
-DEFAULT_DATA_QUALITY_RULESET = """
-    Rules = [
-        ColumnCount > 0
-    ]
-"""
-
-# ---------------------------------------------------
-# Read from MySQL RDS using Glue Connection
-# ---------------------------------------------------
-
-MySQL_node = glueContext.create_dynamic_frame.from_options(
-    connection_type="mysql",
-    connection_options={
-        "useConnectionProperties": "true",
-        "dbtable": "glue_demo",
-        "connectionName": "mysql-rds-glue-connection",
-    },
-    transformation_ctx="MySQL_node"
+secrets_client = boto3.client("secretsmanager")
+secret_response = secrets_client.get_secret_value(
+    SecretId=args["secret_name"]
 )
 
-# ---------------------------------------------------
-# Custom Transform
-# ---------------------------------------------------
+secret = json.loads(secret_response["SecretString"])
 
-CustomTransform_node = MyTransform(
-    glueContext,
-    DynamicFrameCollection(
-        {"MySQL_node": MySQL_node},
-        glueContext
-    )
+jdbc_url = f"jdbc:mysql://{secret['host']}:{secret['port']}/{secret['database']}"
+
+connection_properties = {
+    "user": secret["username"],
+    "password": secret["password"],
+    "driver": "com.mysql.cj.jdbc.Driver"
+}
+
+df = spark.read.jdbc(
+    url=jdbc_url,
+    table=args["db_table"],
+    properties=connection_properties
 )
 
-# ---------------------------------------------------
-# Select From Collection
-# ---------------------------------------------------
+print("MySQL table loaded")
+df.show()
 
-SelectFromCollection_node = SelectFromCollection.apply(
-    dfc=CustomTransform_node,
-    key=list(CustomTransform_node.keys())[0],
-    transformation_ctx="SelectFromCollection_node"
+transformed_df = df.withColumn(
+    "total_price",
+    col("quantity").cast("int") * col("price").cast("double")
 )
 
-# ---------------------------------------------------
-# Data Quality Check
-# ---------------------------------------------------
+transformed_df.show()
 
-EvaluateDataQuality().process_rows(
-    frame=SelectFromCollection_node,
-    ruleset=DEFAULT_DATA_QUALITY_RULESET,
-    publishing_options={
-        "dataQualityEvaluationContext": "EvaluateDataQuality_node",
-        "enableDataQualityResultsPublishing": True
-    },
-    additional_options={
-        "dataQualityResultsPublishing.strategy": "BEST_EFFORT",
-        "observations.scope": "ALL"
-    }
-)
+transformed_df.write \
+    .mode("overwrite") \
+    .option("header", "true") \
+    .csv(args["output_path"])
 
-# ---------------------------------------------------
-# Write to S3
-# ---------------------------------------------------
-
-AmazonS3_node = glueContext.write_dynamic_frame.from_options(
-    frame=SelectFromCollection_node,
-    connection_type="s3",
-    format="glueparquet",
-    connection_options={
-        "path": "s3://glue-etl-mlops-pipeline/output/",
-        "partitionKeys": []
-    },
-    format_options={
-        "compression": "snappy"
-    },
-    transformation_ctx="AmazonS3_node"
-)
+print("Output written to S3")
 
 job.commit()
+
+print("Glue Job Completed Successfully")
